d50f0411a3d5d9d1a43c993652246585548a40d5
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-05-12 16:40:26 +0530

    Completed the Rectangle Class

--- Section 6/Challenge 36. Pool Area/src/com/company/Rectangle.java
+++ Section 6/Challenge 36. Pool Area/src/com/company/Rectangle.java
@@ -1,5 +1,34 @@
 package com.company;
 
 public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if (width <= 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+        if (length <= 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
 
 }

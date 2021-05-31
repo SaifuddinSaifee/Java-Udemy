e2213203b92e5bd1ed56eaeac58acc89718695d0
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-05-31 12:12:42 +0530

    Ended Challenge

---
+++ Section 7/91. Composition/src/com/company/Ceiling.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Ceiling {
+    private int height;
+    private int paintColour;
+
+    public Ceiling(int height, int paintColour) {
+        this.height = height;
+        this.paintColour = paintColour;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintColour() {
+        return paintColour;
+    }
+}
--- Section 7/91. Composition/src/com/company/Main.java
+++ Section 7/91. Composition/src/com/company/Main.java
@@ -3,17 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Dimensions dimensions = new Dimensions(20,20,5);
-        Case theCase = new Case("2208BC","Dell",240,dimensions);
+        // write your code here
+//        Dimensions dimensions = new Dimensions(20,20,5);
+//        Case theCase = new Case("2208BC","Dell",240,dimensions);
+//
+//        Monitor theMonitor = new Monitor ("27 Inch Beast", "Acer",27, new Resolution(2540,1440));
+//
+//        MotherBoard theMotherBoar = new MotherBoard("BJ-200", "Asus",4,6,"v2.44");
+//
+//        PC thePC = new PC (theCase, theMonitor, theMotherBoar);
+//        thePC.powerUp();
 
-        Monitor theMonitor = new Monitor ("27 Inch Beast", "Acer",27, new Resolution(2540,1440));
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("North");
+        Wall wall4 = new Wall("South");
+        Ceiling ceiling = new Ceiling(12, 55);
+        Bed bed = new Bed("Queen",3,8,2,1);
+        Lamp lamp = new Lamp("Loft", true, 4);
+        Bedroom bedroom = new Bedroom("Saif",wall1,wall2,wall3,wall4,ceiling,bed,lamp);
 
-        MotherBoard theMotherBoar = new MotherBoard("BJ-200", "Asus",4,6,"v2.44");
-
-        PC thePC = new PC (theCase, theMonitor, theMotherBoar);
-        thePC.powerUp();
-
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
 
 
     }

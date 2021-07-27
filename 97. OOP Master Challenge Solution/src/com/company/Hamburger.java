bff033464f59eb443f27e34880597e8c5cdfc31c
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-07-27 21:46:48 +0530

    Adding variables to Hamburger
    Adding variables to Hamburger

---
+++ 97. OOP Master Challenge Solution/src/com/company/Hamburger.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Hamburger {
+    private String name;
+    private String meet;
+    private double price;
+    private String breadRollType;
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meet, double price, String breadRollType) {
+        this.name = name;
+        this.meet = meet;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
+    public void addHamburgerAddition1(String name, double price){
+        this.addition1Name = name;
+        this.addition1Price
+    }
+}

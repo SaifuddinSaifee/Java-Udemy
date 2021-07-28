c530f4ed15f0998cd93188bed1f48e67a8b99e5e
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-07-28 16:04:47 +0530

    Added

--- 97. OOP Master Challenge Solution/src/com/company/Hamburger.java
+++ 97. OOP Master Challenge Solution/src/com/company/Hamburger.java
@@ -21,8 +21,30 @@ public class Hamburger {
         this.breadRollType = breadRollType;
     }
 
-    public void addHamburgerAddition1(String name, double price){
+    public void addHamburgerAddition1(String name, double price) {
         this.addition1Name = name;
         this.addition1Price = price;
     }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
+
+    public double itemizeHamburger() {
+        double hamburgerPrice = this.price;
+        System.out.println(this.name + " hamburger " + " on a " + this.breadRollType + " roll " + "price is " + this.price);
+        return 0;
+    }
+
 }
